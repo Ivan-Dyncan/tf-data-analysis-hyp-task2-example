a5cbd251fb1fa424d4b93e2a83153ac47e7b39d3
Ivan-Dyncan <dodzegun@bk.ru>
2023-04-12 21:38:34 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 683312730 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    L = 1.555
+    n1 = len(x)
+    n2 = len(y)
+    m1 = x.mean()
+    m2 = y.mean()
+    std1 = x.std()
+    std2 = y.std()
+    z = (m1-m2)/(std1**2/n1 + std2**2/n2)**(1/2)
+    return abs(z)<L
